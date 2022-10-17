caf43e0b10e202b5faac6dbf4b7f7df3b9d46657
Quan Nguyen <71422583+AllenNguyen0417@users.noreply.github.com>
2022-10-17 15:39:52 -0700

    Adding Inventory Page

--- ecommerce/src/main/java/com/ecommerce/ecommerce/controller/inventory/ProductController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/controller/inventory/ProductController.java
@@ -7,10 +7,10 @@ import com.ecommerce.ecommerce.status.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.json.JSONArray;
 
 @CrossOrigin
 @RestController
